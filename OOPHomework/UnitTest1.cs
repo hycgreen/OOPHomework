6ce28b615d15e3a82609d7153ab6a0deacc65328
hycgreen <hycgreen@gmail.com>
2016-06-03 21:35:38 +0800

    initial commit

---
+++ OOPHomework/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OOPHomeworkTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Man_BMI_is_less_than_20
+        {
+        }
+    }
+}
