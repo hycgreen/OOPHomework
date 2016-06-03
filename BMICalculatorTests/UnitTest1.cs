279d56386b9baf3b074a0ed40115ee9487359625
hycgreen <hycgreen@gmail.com>
2016-06-04 00:30:53 +0800

    加入第3個案例 Man 中間值結果字串為 “適中 ”

--- BMICalculatorTests/UnitTest1.cs
+++ BMICalculatorTests/UnitTest1.cs
@@ -49,5 +49,26 @@ namespace BMICalculatorTests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void Test_Man_BMI_Between_20_And_25_ResultString_Should_Be_Normal()
+        {
+            //arrange
+            BMIInformation info = new BMIInformation();
+            info.Gender = Gender.Male;
+            info.Height = 1.75;
+            info.Wight = 80;
+
+            IBMICalculator target = BMICalculatorFactory.GetBMICalculator(info);
+
+            //act
+            target.Calculate();
+
+            string actual = info.Recommendation;
+            string expected = "適中";
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
